3bfd5a35669368d20da81a8a0617de22e4f0ad1a
Mohammed Mujtaba <mohamedmujtaba07@gmail.com>
2025-01-10 20:10:42 +0530

    Update Shop.js

--- src/Pages/Shop.js
+++ src/Pages/Shop.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useDispatch } from 'react-redux';
 // import React, { useState } from 'react';
 // import { addToCart, removeFromCart } from '../Store/cartSlice';
