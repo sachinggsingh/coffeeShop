29c8fca2b4dee848e278bea60f4ac880bb3a88cf
Jivan <137729176+Jivan052@users.noreply.github.com>
2025-01-10 17:43:57 +0530

    Update soup.js

--- src/Pages/soup.js
+++ src/Pages/soup.js
@@ -56,7 +56,7 @@ const ProductName = styled.h3`
 `;
 
 const ProductPrice = styled.p`
-  font-size: 1.5rem;
+  font-size: 1.1rem;
   color: #4a2c2a;
   margin-bottom: 1rem;
   font-weight: 600;
@@ -104,164 +104,15 @@ const StyledButton = styled.button`
   box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
   transition: background 0.3s ease, transform 0.2s ease;
 
-  .CartBtn {
-    width: 140px;
-    height: 40px;
-    border-radius: 12px;
-    border: none;
-    background-color: rgb(255, 208, 0);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-    transition-duration: .5s;
-    overflow: hidden;
-    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.103);
-    position: relative;
+  &:hover {
+    background: linear-gradient(145deg, #7d5858, #8e6a6a);
+    transform: scale(1.05);
   }
-
-  .IconContainer {
-    position: absolute;
-    left: -50px;
-    width: 30px;
-    height: 30px;
-    background-color: transparent;
-    border-radius: 50%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-    z-index: 2;
-    transition-duration: .5s;
+    
+  &:active {
+    transform: scale(0.98);
+    box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.3); 
   }
-
-  .icon {
-    border-radius: 1px;
-  }
-
-  .text {
-    height: 100%;
-    width: fit-content;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: rgb(17, 17, 17);
-    z-index: 1;
-    transition-duration: .5s;
-    font-size: 1.04em;
-    font-weight: 600;
-  }
-
-  .CartBtn:hover .IconContainer {
-    transform: translateX(58px);
-    border-radius: 40px;
-    transition-duration: .5s;
-  }
-
-  .CartBtn:hover .text {
-    transform: translate(10px,0px);
-    transition-duration: .5s;
-  }
-
-  .CartBtn:active {
-    transform: scale(0.95);
-    transition-duration: .5s;
-  }
-`;
-
-const StyledWrapper = styled.div`
-  .heart-container {
-    --heart-color: maroon;
-    position: relative;
-    width: 50px;
-    height: 40px;
-    transition: .3s;
-    z-index:100;
-  }
-
-  .heart-container .checkbox {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    opacity: 0;
-    z-index: 20;
-    cursor: pointer;
-  }
-
-  .heart-container .svg-container {
-    width:100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .heart-container .svg-outline,
-  .heart-container .svg-filled {
-    fill: var(--heart-color);
-    position: absolute;
-  }
-
-  .heart-container .svg-filled {
-    animation: keyframes-svg-filled 1s;
-    display: none;
-  }
-
-  .heart-container .svg-celebrate {
-    position: absolute;
-    animation: keyframes-svg-celebrate .5s;
-    animation-fill-mode: forwards;
-    display: none;
-    stroke: var(--heart-color);
-    fill: var(--heart-color);
-    stroke-width: 2px;
-  }
-
-  .heart-container .checkbox:checked~.svg-container .svg-filled {
-    display: block
-  }
-
-  .heart-container .checkbox:checked~.svg-container .svg-celebrate {
-    display: block
-  }
-
-  @keyframes keyframes-svg-filled {
-    0% {
-      transform: scale(0);
-    }
-
-    25% {
-      transform: scale(1.2);
-    }
-
-    50% {
-      transform: scale(1);
-      filter: brightness(1.5);
-    }
-  }
-
-  @keyframes keyframes-svg-celebrate {
-    0% {
-      transform: scale(0);
-    }
-
-    50% {
-      opacity: 1;
-      filter: brightness(1.5);
-    }
-
-    100% {
-      transform: scale(1.4);
-      opacity: 0;
-      display: none;
-    }
-  }
-`;
-
-const ProductInfo1 = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
 `;
 
 const QuantityControls = styled.div`
@@ -281,7 +132,48 @@ const QuantityDisplay = styled.span`
   min-width: 20px;
   text-align: center;
 `;
-const products = [
+
+function Soup() {
+  const dispatch = useDispatch();
+  const [quantities, setQuantities] = useState({});
+
+  const handleAddToCart = (product) => {
+    if (!quantities[product.id]) {
+      setQuantities({
+        ...quantities,
+        [product.id]: 1
+      });
+      dispatch(addToCart({ ...product, quantity: 1 }));
+    }
+  };
+
+  const handleIncrement = (product) => {
+    const newQuantity = (quantities[product.id] || 0) + 1;
+    setQuantities({
+      ...quantities,
+      [product.id]: newQuantity
+    });
+    dispatch(addToCart({ ...product, quantity: newQuantity }));
+  };
+
+  const handleDecrement = (product) => {
+    if (quantities[product.id] > 1) {
+      const newQuantity = quantities[product.id] - 1;
+      setQuantities({
+        ...quantities,
+        [product.id]: newQuantity
+      });
+      dispatch(addToCart({ ...product, quantity: newQuantity }));
+    } else {
+      setQuantities({
+        ...quantities,
+        [product.id]: 0
+      });
+      dispatch(removeFromCart(product.id));
+    }
+  };
+
+  const products = [
     {
         id: 42,
         name: "Salad",
@@ -338,55 +230,16 @@ const products = [
       },
 ];
 
-function Soup() {
-  const dispatch = useDispatch();
-  const [quantities, setQuantities] = useState({});
-
-  const handleAddToCart = (product) => {
-    if (!quantities[product.id]) {
-      setQuantities({
-        ...quantities,
-        [product.id]: 1
-      });
-      dispatch(addToCart({ ...product, quantity: 1 }));
-    }
-  };
-
-  const handleIncrement = (product) => {
-    const newQuantity = (quantities[product.id] || 0) + 1;
-    setQuantities({
-      ...quantities,
-      [product.id]: newQuantity
-    });
-    dispatch(addToCart({ ...product, quantity: newQuantity }));
-  };
-
-  const handleDecrement = (product) => {
-    if (quantities[product.id] > 1) {
-      const newQuantity = quantities[product.id] - 1;
-      setQuantities({
-        ...quantities,
-        [product.id]: newQuantity
-      });
-      dispatch(addToCart({ ...product, quantity: newQuantity }));
-    } else {
-      setQuantities({
-        ...quantities,
-        [product.id]: 0
-      });
-      dispatch(removeFromCart(product.id));
-    }
-  };
 
   return (
     <SoupContainer>
       <Title
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        Our Soupshake Selection
-      </Title>
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+         Our Soupshake Selection
+        </Title>
       <ProductGrid>
         {products.map((product) => (
           <ProductCard key={product.id}>
@@ -398,6 +251,7 @@ function Soup() {
                 <Button onClick={() => handleAddToCart(product)}>
                   Add to Cart
                 </Button>
+            
               ) : (
                 <QuantityControls>
                   <QuantityButton onClick={() => handleDecrement(product)}>-</QuantityButton>
@@ -415,5 +269,3 @@ function Soup() {
 }
 
 export default Soup;
-  
-
